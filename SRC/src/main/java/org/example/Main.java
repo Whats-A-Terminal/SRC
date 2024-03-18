5885c89bcfdf4e6b66052c7ffaa5a5a25b597086
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2024-03-18 15:05:33 -0700

    Added basic navigation
    The navigation does not work, but the outline has been created. The case statements will be changed to the actual function later on!

--- SRC/src/main/java/org/example/Main.java
+++ SRC/src/main/java/org/example/Main.java
@@ -1,7 +1,59 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class Main {
+
+    public static void mainMenu(){
+
+        Scanner input = new Scanner(System.in);
+        while (true){
+            System.out.println("\nMain Menu: ");
+            System.out.println("1: See all crops");
+            System.out.println("2: Add new crop");
+            System.out.println("3: Edit crop");
+            System.out.println("4: Delete crop");
+            System.out.println("5: PUSH ALL CHANGES");
+            System.out.println("0: QUIT PROGRAM");
+
+            System.out.print("\nEnter option: ");
+
+            int userInput = input.nextInt();
+
+            switch (userInput){
+                case 0:
+                    System.exit(0);
+                    break;
+
+                case 1:
+                    System.out.println("\nShowing you all crops...");
+                    break;
+
+                case 2:
+                    System.out.println("\nAdding new crop...");
+                    break;
+
+                case 3:
+                    System.out.println("\nEditing crop...");
+                    break;
+
+                case 4:
+                    System.out.println("\nDeleting crop...");
+                    break;
+
+                case 5:
+                    System.out.println("\nPUSHING CHANGES...");
+                    break;
+
+                default:
+                    System.out.println("Invalid option! Please try again.");
+                    break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        mainMenu();
     }
 }
